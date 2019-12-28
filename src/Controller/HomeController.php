3878ec9e35a2f76f8aa0b5c9db7889a777f0a301
JohJohan <johan.vlaar.1994@gmail.com>
2019-12-28 16:49:12 +0100

    Add home template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -15,8 +16,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): string
+    public function home(): Response
     {
-        return 'Hello World';
+        return $this->render('home/home.html.twig');
     }
 }
